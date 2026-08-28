46be40fa1a633b12e6a17059e88524aa10a25b4b
Mingo222 <doming0202@gmail.com>
2026-08-28 18:50:05 +0900

    Enable timeline click seeking and pins

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
 import './volume-control'
+import './timeline-controls'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
