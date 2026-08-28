7c4b4c3cc2a4970c853fe389c75eabd7fb5dc7c9
Mingo222 <doming0202@gmail.com>
2026-08-28 21:42:33 +0900

    Require palette selection before starting game

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,24 @@
+import { useState } from 'react'
 import './keyboard-startup-buffer'
 import './keyboard-guide'
 import './title-reset.css'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
+import { PaletteStartModal } from './components/PaletteStartModal'
 import './volume-control'
 import './pause-overlay'
 import './timeline-controls'
 import './timeline-resume'
 import './timeline-branch'
 
+function Startup() {
+  const [started, setStarted] = useState(false)
+  return started ? <App /> : <PaletteStartModal onStart={() => setStarted(true)} />
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Startup />
   </StrictMode>,
 )
