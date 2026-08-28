5e28ab16c29844c93a250fc481a151260c436370
Mingo222 <doming0202@gmail.com>
2026-08-29 08:32:10 +0900

    feat: show fall speed control in center gap

--- src/main.tsx
+++ src/main.tsx
@@ -4,14 +4,17 @@ import './title-reset.css'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
+import { FallSpeedControl } from './components/FallSpeedControl'
 import './volume-control'
 import './pause-overlay'
 import './timeline-controls'
 import './timeline-resume'
 import './timeline-branch'
+import './fall-speed.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <FallSpeedControl />
   </StrictMode>,
 )
